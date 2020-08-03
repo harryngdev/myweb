9568a12049568609122a607fbb52298135348fc5
Nguyễn Văn Hà <nguyenvanha111120@gmail.com>
2020-08-03 14:05:36 +0700

    update 6.2

--- index/js/main.js
+++ index/js/main.js
@@ -1,5 +1,5 @@
 function toggleMenu(){
-    let header__menu = document.querySelector('.header__menu');
+    let header__menu = document.querySelector('.header__menu--res');
     let toggle = document.querySelector('.toggle');
     header__menu.classList.toggle('active');
     toggle.classList.toggle('active');
